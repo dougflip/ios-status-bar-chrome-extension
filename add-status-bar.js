e29342ceeebfbb64d32aae7b71d5db249413d21d
Carl Paradis <paresthesia69@gmail.com>
2016-04-01 16:27:59 -0400

    Emulating the ios status bar with ionicons and javascript date
    This fixes Issue #4

--- add-status-bar.js
+++ add-status-bar.js
@@ -1,6 +1,30 @@
-const addIOSStatusBar = () => {
+const formatIOSStatusBarTime = (date) => {
+        let hours = date.getHours(),
+            minutes = date.getMinutes(),
+            ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        return hours + ':' + minutes + ' ' + ampm;
+    },
+    getIOSStatusBarHTML = () => {
+        return '<span class="ios-status-bar-overlay-left">' +
+                '<i class="circle circle-filled"></i>' +
+                '<i class="circle circle-filled"></i>' +
+                '<i class="circle"></i>' +
+                '<i class="circle"></i>' +
+                '<i class="circle"></i>' +
+            '</span>' +
+            '<span class="ios-status-bar-overlay-center">'+formatIOSStatusBarTime(new Date())+'</span>' +
+            '<span class="ios-status-bar-overlay-right">' +
+                '<i class="icon ion-bluetooth"></i>' +
+                '<i class="icon ion-battery-half"></i>' +
+            '</span>';
+    },
+    addIOSStatusBar = () => {
         const iosStatusBar = document.createElement('div');
         iosStatusBar.classList.add('ios-status-bar-overlay');
+        iosStatusBar.innerHTML = getIOSStatusBarHTML();
         document.body.appendChild(iosStatusBar);
         document.body.classList.remove('platform-browser');
         document.body.classList.add('platform-cordova');
@@ -10,13 +34,13 @@ const addIOSStatusBar = () => {
         return (document.body.classList && document.body.classList.contains(classStr));
     },
     resetIOSStatusBar = () => {
-        if(bodyClassContains('platform-cordova')) {
+        if (bodyClassContains('platform-cordova')) {
             document.body.classList.remove('platform-cordova');
             document.body.classList.add('platform-browser');
         }
 
         let iosStatusBarElements = document.getElementsByClassName('ios-status-bar-overlay');
-        if(iosStatusBarElements.length > 0) {
+        if (iosStatusBarElements.length > 0) {
             iosStatusBarElements[0].parentNode.removeChild(iosStatusBarElements[0]);
         }
     },
