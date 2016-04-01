e2c7e59ca8f5acb4ff3808363d4e8be49640056e
Carl Paradis <paresthesia69@gmail.com>
2016-03-31 20:17:30 -0400

    Allowing modes to fix Issue #5
    Defaults to disabled mode.
    Added more matches for extension permissions
    Adding icon to fix Issue #2

--- add-status-bar.js
+++ add-status-bar.js
@@ -1,7 +1,6 @@
-//FIXME: Need a way to set currentStatusBarMode
-const currentStatusBarMode = 'detect-platform-ios',
-    addStatusBar = () => {
-        console.log('iOS Status Bar Overlay', 'currentStatusBarMode: ' + currentStatusBarMode);
+let currentIOSStatusBarMode = 'disabled';//'detect-platform-ios','force','disabled'
+const addStatusBar = () => {
+        console.log('iOSStatusBarOverlay', 'currentIOSStatusBarMode: ' + currentIOSStatusBarMode);
         const bar = document.createElement('div');
         bar.classList.add('ios-status-bar');
         bar.style.backgroundColor = 'rgba(0, 0, 0, .5)';
@@ -14,12 +13,27 @@ const currentStatusBarMode = 'detect-platform-ios',
         document.body.classList.remove('platform-browser');
         document.body.classList.add('platform-cordova');
     },
+    resetStatusBar = () => {
+        document.body.classList.remove('platform-cordova');
+        document.body.classList.add('platform-browser');
+
+        let statusBarElements = document.getElementsByClassName('ios-status-bar');
+        if(statusBarElements.length > 0) {
+            statusBarElements[0].parentNode.removeChild(statusBarElements[0]);
+        }
+    },
     isPlatformIOS = () => {
         return (document.body.classList && document.body.classList.contains('platform-ios'));
+    },
+    setIOSStatusBarMode = (newMode) => {
+        console.log('iOSStatusBarOverlay setIOSStatusBarMode', 'currentIOSStatusBarMode: ' + newMode);
+        currentIOSStatusBarMode = newMode;
+        resetStatusBar(newMode);
+        if ((currentIOSStatusBarMode === 'detect-platform-ios') && isPlatformIOS()) {
+            addStatusBar();
+        } else if (currentIOSStatusBarMode === 'force') {
+            addStatusBar();
+        }
     };
 
-if ((currentStatusBarMode === 'detect-platform-ios') && isPlatformIOS()) {
-    addStatusBar();
-} else if (currentStatusBarMode === 'force') {
-    addStatusBar();
-}
+setIOSStatusBarMode('disabled');
